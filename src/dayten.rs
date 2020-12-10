7303bcad87795b48e194c5563e4534e8535e69fa
hafrei <h.a.freiburger@gmail.com>
2020-12-10 08:36:11 -0500

    Set up for day 10

---
+++ src/dayten.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+pub fn execute_dayten() {
+  let path = "./input/day10.txt";
+  let working = prepare_input(path);
+}
+
+fn prepare_input (filepath: &str) -> Vec<String> {
+  let mut ret: Vec<String> = Vec::new();
+  let list = fs::read_to_string(filepath).expect("Yeah, that's not a file");
+
+  for lin in list.lines() {
+    ret.push(lin.to_string().clone());
+  }
+  return ret;
+}
