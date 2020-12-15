a6654af464ec1b0f78c0299f0fe8c1a99c4bcb47
hafrei <h.a.freiburger@gmail.com>
2020-12-15 01:05:44 -0500

    Renamed variables and some other more sensible changes

--- src/dayten.rs
+++ src/dayten.rs
@@ -5,7 +5,7 @@ use itertools::Itertools;
 pub fn execute_dayten() {
   let path = "./input/day10.txt";
   let working: Vec<i32> = prepare_input(path);
-  let (one, three) = kinda_cheeky(working.clone());
+  let (one, three) = gap_count(working.clone());
   println!("Should be {} * {} = {}", one, three, one * three );
 
   let perms = get_permutations(working);
@@ -31,33 +31,37 @@ fn get_permutations(full_list: Vec<i32>) -> i64{
   So, in terms of permutations, you need to know the total number of permutations
   And then you need to know the minimum allowed sequence of adaptors
   So, maybe you could do an array of the gaps and get the permutations of all the gaps/potential gaps
+  0..x
+  Need to know if the neighbors can be removed before considering each number's individual permutation
+  Worst case scenario and every gap is 1, the range needs to be 3 in either direction, if exists
+  0 and <create last number> cannot be removed
  */
 
- fn kinda_cheeky(worklist: Vec<i32>) -> (i32, i32){ //2170 too low 6300 too high
+ fn gap_count(worklist: Vec<i32>) -> (i32, i32){ //2170 too low 6300 too high
    let mut workan: Vec<i32> = worklist;
    workan.sort_unstable();
+   workan.push(workan.last().unwrap()+3); //The laptop
    let mut one = 0;
    let mut three = 0;
 
    for (e, &x) in workan.iter().enumerate() {
-    let come_on: Vec<i32>  = workan.clone();
-     let fine: usize = e+1;
-     if fine >= workan.len() {
+    let searcher: Vec<i32>  = workan.clone();
+     let i_target: usize = e+1;
+     if i_target >= workan.len() {
        break;
      }
-     let okay: i32 = come_on[fine];
-     if okay - x == 1 {
+     let target: i32 = searcher[i_target];
+     if target - x == 1 {
        one +=1;
-     } else if okay - x == 2 {
+     } else if target - x == 2 {
       println!("Sommin");
      }
-     else if okay - x == 3{
+     else if target - x == 3{
       three +=1;
      } else {
-       println!("a broke?");
+       println!("Oh man, not sure how we got here");
      }
    }
-   three += 1; //Cause your laptop!
 
    (one as i32, three as i32)
  }
