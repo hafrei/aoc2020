482dace56f23d6304fcdc4aa5797e08c64945ff2
hafrei <h.a.freiburger@gmail.com>
2020-12-22 21:38:16 -0500

    Creating checkpoint before I become too invested in this new idea

--- src/dayeleven.rs
+++ src/dayeleven.rs
@@ -3,9 +3,11 @@ use std::fs;
 pub fn execute_dayeleven(){
   let path = "./input/day11.txt";
   let working= prepare_input(path);
+  let occupied_seats = find_stable_occupied(working);
+  println!("Waiting");
 }
 
-/* Look into traits? 
+/* 
   . Unoccupied; never changes
    L if not occupied and no adjacent occupied seats
    # occupied. If 4 adjacent seats are occupied, it becomes unoccupied
@@ -14,14 +16,43 @@ pub fn execute_dayeleven(){
    Count the number of occupied seats
 */
 
+fn find_stable_occupied(layout: Vec<String>) -> i32 {
+  let mut active_layout = layout.clone();
+  let mut occupied = 0;
+  let mut iterated = 0;
+
+  loop {
+    let new_layout = active_layout.clone();
+    let prev_occ = 0;
+
+    for (ina, r) in active_layout.clone().iter().enumerate() {
+      for (inb, c) in r.char_indices() {
+        match c {
+          '.' => println!("Floor"),
+          'L' => println!("Open"),
+          '#' => println!("Occupied"),
+          _ => println!("Not sure what happened here")
+        }
+      }
+    }
+
+    if prev_occ.eq(&occupied) {
+      break;
+    }
+  }
+  occupied
+}
+
 
 fn prepare_input (filepath: &str) -> Vec<String> {
   let list = fs::read_to_string(filepath).expect("Yeah, that's not a file");
-  let ret: Vec<String> = list
+  let ret= list
                       .as_str()
                       .split('\n')
                       .map(str::parse::<String>)
                       .map(Result::unwrap)
                       .collect();
+
+    
   ret
 }
