174367e4e9dd486d7727290b82131459f572f207
hafrei <h.a.freiburger@gmail.com>
2020-12-24 00:41:32 -0500

    I think I'm going off on the wrong path so I'm going to have a think

--- src/dayeleven.rs
+++ src/dayeleven.rs
@@ -4,30 +4,30 @@ use std::io::prelude::*;
 use std::io::Error;
 use std::io::LineWriter;
 
-
 pub fn execute_dayeleven(){
-  let path = "./input/day11.txt";
+  let path = "./input/day11test.txt";
   let working= prepare_input(path);
-  let occupied_seats = find_stable_occupied(working); // 2164 is too low
-  println!("There are {} occupied seats at stability", occupied_seats);
+  let expected_occupied_seats = find_stable_occupied(working.clone());
+  println!("There are {} occupied seats at stability", expected_occupied_seats);
+  let actual_occupied_seats = find_real_stable_occupied(working);
+  println!("Nope, I was wrong. There's actually {} occupied seats", actual_occupied_seats);
 }
 
 /* 
   . Unoccupied; never changes
    L if not occupied and no adjacent occupied seats
-   # occupied. If 4 adjacent seats are occupied, it becomes unoccupied
+   # occupied. If 5 seats are occupied in any of the 8 directions, it becomes unoccupied
 
    Apply these rules until no more changes
    Count the number of occupied seats
 */
-
-fn find_stable_occupied(layout: Vec<Vec<u8>>) -> i32 {
+fn find_real_stable_occupied(layout: Vec<Vec<u8>>) -> i32 {
   let mut active_layout = layout; // Used as iterator 2D Vector for each round
   let threshold = 4; // A seat becomes available if 4 or more adjacent seats are occupied
   let mut occupied = 0; // Counts number of occupied seats for this round
   let mut prev_occ = 0; // Holds the number of occupied seats from last round
   let mut total_occupied: i32 = 0; // Return value, recieves sum from active_layout's occupied seat count
-  let mut counter = 0; // Used for creating the output file
+  //let mut counter = 0; // Used for creating the output file
 
   loop {
     let mut new_layout = active_layout.clone(); 
@@ -38,12 +38,152 @@ fn find_stable_occupied(layout: Vec<Vec<u8>>) -> i32 {
       let mut total_available = 0;
       let area_length = active_layout.len() -1;
 
-      let olr = match ena { //outer lower range
+      //enb = enumerator b, Index of current row
+      for (enb, c) in r.iter().enumerate() {
+        let row_length = r.len() -1;
+        let ta_reset = total_available;
+        let mut counted = 0;
+        let mut looking_for = 0; //0 is value idk, 1 is value of open, 2 is the value of occupied
+
+        //What is current seat?
+        looking_for = check_seat(*c, looking_for) as u8;
+
+        // Don't bother running everything for hallway space
+        if looking_for.eq(&3) {
+          continue;
+        }
+
+        //Look down the current row for occupied seats to the left
+        if !enb.eq(&0) {
+          let found_maybe = look_left(enb, r, looking_for);
+          counted += found_maybe;
+          total_available += found_maybe;
+        }
+        // and then to the right
+        if !enb.eq(&row_length) {
+          let found_maybe = look_right(enb, r, looking_for, row_length);
+          counted += found_maybe;
+          total_available += found_maybe;
+        }
+
+        // Check the row above (lower number) for occupied seats
+        //TODO: How about, instead, you find which is bigger: 
+        // ena/enb, 0 or area_length or row_length
+        // And then on each loop, subtract an interator with that number from the direction
+        if !ena.eq(&0) {
+          //Look directly above current seat
+          for lu in ena..=0 {
+            if check_seat(active_layout[lu][enb], looking_for) == 1 {
+              counted += 1;
+              total_available += 1;
+              break;
+            }
+          }
+
+          // then above left
+          if !enb.eq(&0) {
+            for ll in enb..=0 {
+              if check_seat(active_layout[ll][enb], looking_for) == 1 {
+                counted += 1;
+                total_available += 1;
+                break;
+              }
+            }
+            
+          }
+          // then above right
+          if !enb.eq(&row_length) {
+            let look_up_right: Vec<usize> = (enb+1..=row_length).collect();
+            for lr in look_up_right {
+              if check_seat(active_layout[lr][enb], looking_for) == 1 {
+                counted += 1;
+                total_available += 1;
+                break;
+              }
+            }
+          }
+
+        }
+        // Check the row below (higher number) for occupied seats
+        if !ena.eq(&area_length) {
+          let look_down: Vec<usize> = (ena..=area_length).collect();
+          for ld in look_down {
+            if check_seat(active_layout[ld][enb], looking_for) == 1 {
+              counted += 1;
+              total_available += 1;
+              break;
+            }
+          }
+        }
+
+        //If we are currently counting up open seats and enough are vacant, sit down
+        //If we are currently counting up occupied seats and enough are full
+        if looking_for.eq(&1) && counted.eq(&(total_available as i32)) {
+          new_layout[ena][enb] = b'#';
+          occupied +=1;
+        } else if looking_for.eq(&2) && counted.ge(&threshold) {
+          new_layout[ena][enb] = b'L';
+          occupied -=1;
+        }
+
+        total_available = ta_reset;
+      }
+    }
+
+    //Write current grid to file
+    //write_iter(new_layout.clone(), counter);
+    //counter += 1; // Since we'll be writing every iteration
+
+    // Since we are no longer iterating over active layout we can update it with the changes
+    active_layout = new_layout;
+
+    //Finally, if there have been no changes from this round and last round
+    // then count the number of occupied seats and break out of the loop
+    if prev_occ.eq(&occupied) { 
+      total_occupied += active_layout.iter()
+                              .flatten()
+                              .map(|x| if x.eq(&b'#') {1} else {0})
+                              .sum::<i32>();
+      break;
+    } else {
+      prev_occ = occupied;
+    }
+  }
+  total_occupied
+}
+
+/* 
+  . Unoccupied; never changes
+   L if not occupied and no adjacent occupied seats
+   # occupied. If 4 adjacent seats are occupied, it becomes unoccupied
+
+   Apply these rules until no more changes
+   Count the number of occupied seats
+*/
+fn find_stable_occupied(layout: Vec<Vec<u8>>) -> i32 {
+  let mut active_layout = layout; // Used as iterator 2D Vector for each round
+  let threshold = 4; // A seat becomes available if 4 or more adjacent seats are occupied
+  let mut occupied = 0; // Counts number of occupied seats for this round
+  let mut prev_occ = 0; // Holds the number of occupied seats from last round
+  let mut total_occupied: i32 = 0; // Return value, recieves sum from active_layout's occupied seat count
+
+  loop {
+    let mut new_layout = active_layout.clone(); 
+
+    //ena = Enumerator A, counter of the whole grid
+    // r = row of grid
+    for (ena, r) in active_layout.iter().enumerate() {
+      let mut total_available = 0;
+      let area_length = active_layout.len() -1;
+
+      //outer lower range
+      let olr = match ena {
         0 => 0,
         _ => ena - 1
       };
 
-      let our = match area_length.eq(&ena) { //outer upper range
+      //outer upper range
+      let our = match area_length.eq(&ena) {
         true => ena,
         false => ena + 1
       };
@@ -55,7 +195,7 @@ fn find_stable_occupied(layout: Vec<Vec<u8>>) -> i32 {
         let mut counted = 0;
         let mut looking_for = 0; //0 is value idk, 1 is value of open, 2 is the value of occupied
 
-        //What is current seat?
+        //What is current space? Let's take a look so we know what we're doing
         looking_for = check_seat(*c, looking_for) as u8;
 
         // Don't bother running everything for hallway space
@@ -104,7 +244,7 @@ fn find_stable_occupied(layout: Vec<Vec<u8>>) -> i32 {
         }
 
         //If we are currently counting up open seats and enough are vacant, sit down
-        //If we are currently counting up occupied seats and enough are full
+        //If we are currently counting up occupied seats and enough are full, get up
         if looking_for.eq(&1) && counted.eq(&(total_available as i32)) {
           new_layout[ena][enb] = b'#';
           occupied +=1;
@@ -116,17 +256,13 @@ fn find_stable_occupied(layout: Vec<Vec<u8>>) -> i32 {
         total_available = ta_reset;
       }
     }
-    
-    //Write current grid to file
-    write_iter(new_layout.clone(), counter);
-    counter += 1; // Since we'll be writing every iteration
 
-        // Since we are no longer iterating over active layout we can update it with the changes
+    // Since we are no longer iterating over active layout we can update it with the changes
     active_layout = new_layout;
-    //Finally, if there have been no changes from this round and last round
+
+    // Finally, if there have been no changes from this round and last round
     // then count the number of occupied seats and break out of the loop
-    if prev_occ.eq(&occupied) { //This no longer works properly
-      //println!("{:?}", active_layout);
+    if prev_occ.eq(&occupied) { 
       total_occupied += active_layout.iter()
                               .flatten()
                               .map(|x| if x.eq(&b'#') {1} else {0})
@@ -134,11 +270,42 @@ fn find_stable_occupied(layout: Vec<Vec<u8>>) -> i32 {
       break;
     } else {
       prev_occ = occupied;
-      //occupied = 0; Occupied now decrements so I shouldn't need this anymore
     }
   }
   total_occupied
 }
+
+// Find the first occurance of the seat you're looking for on the right
+fn look_right(enb: usize, r: &[u8], looking_for: u8, row_length: usize) -> i32{
+  let look_right = &r[enb+1..=row_length];
+  let mut ret = 0;
+
+  for lr in look_right {
+    if check_seat(*lr, looking_for) == 1 {
+      ret = 1;
+      break;
+    }
+  }
+
+  ret
+}
+
+// Find the first occurance of the seat you're looking for on the left
+fn look_left(enb: usize, r: &[u8], looking_for: u8) -> i32{
+  let mut look_left: Vec<usize> = (0..enb).collect();
+  let mut ret = 0;
+
+  look_left.reverse();
+  for ll in look_left {
+    if check_seat(r[ll], looking_for) == 1 {
+      ret = 1;
+      break;
+    }
+  }
+  
+  ret
+}
+
 /**
  * Returns 1 if seat matches L, #, or other
  * l_f = 0 will return 1 for L, 2 for #, and 3 for other
